634f6a2a6052be830e21802480197858583cbc23
Riccardo <gabririchi@yahoo.it>
2024-01-26 21:58:07 +0100

    health component

--- components/HealthComponent.gd
+++ components/HealthComponent.gd
@@ -1,5 +1,9 @@
 extends Node
 
+signal took_damage(old_value, new_value)
+signal healed_damage(old_value, new_value)
+signal is_dead
+
 var hp: int = 100
 @export var maxHp: int = 100
 
@@ -11,3 +15,24 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func take_damage(amount: int):
+	if amount <= 0:
+		return
+	
+	var old_hp = hp
+	hp -= amount
+	
+	if hp <= 0:
+		is_dead.emit()
+	
+	took_damage.emit(old_hp, hp)
+
+func heal_damage(amount: int):
+	if amount <= 0:
+		return
+	
+	var old_hp = hp
+	hp += amount
+	
+	healed_damage.emit(old_hp, hp)
