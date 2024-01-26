221dbe0cf844ceff87a396b123032aacff20bc1d
Riccardo <gabririchi@yahoo.it>
2024-01-26 21:46:30 +0100

    Refactoring health component

---
+++ components/HealthComponent.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var hp: int = 100
+@export var maxHp: int = 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hp = maxHp
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
