bc1c68a88033a6b2be5e531f6ce2eacf6a4bcf09
Riccardo <gabririchi@yahoo.it>
2024-01-26 23:26:45 +0100

    Navigation start implementation

---
+++ game.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	$Infermiera.set_movement_target($Player.global_position)
