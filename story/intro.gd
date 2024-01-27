239fac7b77b8e4c6611219af16c73bdae328e9bc
noraron <noraron-gitlab.smashup894@simplelogin.com>
2024-01-27 12:58:32 +0100

    #4 Progress on intro

--- story/intro.gd
+++ story/intro.gd
@@ -1,22 +1,32 @@
 extends Control
 
-const nero = preload("res://assets/intro/nero.png")
-
 var slides = [
-	Slide.new("Text 1", nero),
-	Slide.new("Text 2", nero),
-	Slide.new("Text 3", nero),
-	Slide.new("Text 4", nero),
-	Slide.new("Text 5", nero)
+	Slide.new("Text 1", preload("res://assets/intro/s1.png")),
+	Slide.new("Text 2", preload("res://assets/intro/s2.png")),
+	Slide.new("Text 3", preload("res://assets/intro/s3.png")),
 ]
 
 var current = 0
+var elapsed = 0
 
 func _ready():
-	pass
+	#$SlideImage.texture = slides[current].image
+	$NarrationText.text = slides[current].text
 	
-func _on_next_pressed():
-	if (current + 1 > len(slides)):
-		SceneManager.goto_scene("res://game.tscn")
-	else:
-		current += 1
+func _process(delta):
+	check_next()
+	show_text(delta)
+
+func check_next():
+	if Input.is_action_pressed("next"):
+		if (current + 1 > len(slides)):
+			SceneManager.goto_scene("res://game.tscn")
+		else:
+			current += 1
+			elapsed = 0
+			#$SlideImage.texture = slides[current].image
+			$NarrationText.text = slides[current].text
+
+func show_text(delta):
+	elapsed += delta
+	$NarrationText.visible_characters = elapsed/0.08
